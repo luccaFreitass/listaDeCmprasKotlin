749f27dc103c5f7fb8ee05d2d758a16fc8332f83
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-22 22:19:39 -0300

    first commit

---
+++ app/src/main/java/br/com/fiap/listadecompras/MainActivity.kt
@@ -0,0 +1,20 @@
+package br.com.fiap.listadecompras
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Lista de Compras"
+    }
+}
+
